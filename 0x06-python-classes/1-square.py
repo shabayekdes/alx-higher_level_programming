a311d43615dfe9870d3d1054eb5f441f0e26e41b
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-09-26 23:45:56 +0300

    Add square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defining a class square """
+
+
+class Square:
+    """ Defining a class square """
+    def __init__(self, size):
+        """Constructor of the Square"""
+        self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -1,4 +1,5 @@
 # 0x06. Python - Classes and Objects
 
 0. My first square
+1. Square with size
 
