dcd30e9f5a1031b83b14e8e5fc4430fc306f7d7f
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-09-28 02:11:47 +0300

    Fix style code

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,7 +8,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
         print("Inside result: {}".format(result))
-    except:
+    except ZeroDivisionError:
         print("Inside result: {}".format(result))
     finally:
         return result
