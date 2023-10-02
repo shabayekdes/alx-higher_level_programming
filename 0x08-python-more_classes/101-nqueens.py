a53e36a15f04b716aa01d81ee184856226e11969
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-10-03 02:16:59 +0300

    format

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
 import sys
 
+
 def is_valid(board, row, col, N):
-    # Check if there is a queen in the same column
+    """ Check if there is a queen in the same column
+    """
     for i in range(row):
         if board[i][col] == 1:
             return False
 
-    # Check upper diagonal on the left side
+    """ Check upper diagonal on the left side
+    """
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
-    # Check upper diagonal on the right side
+    """ Check upper diagonal on the right side
+    """
     for i, j in zip(range(row, -1, -1), range(col, N)):
         if board[i][j] == 1:
             return False
 
     return True
 
+
 def solve_nqueens_util(board, row, N, solutions):
     if row == N:
-        solutions.append([[i, j] for i in range(N) for j in range(N) if board[i][j]])
+        solutions.append(
+            [[i, j] for i in range(N) for j in range(N) if board[i][j]])
+
         return
 
     for col in range(N):
@@ -29,6 +36,7 @@ def solve_nqueens_util(board, row, N, solutions):
             board[row][col] = 1
             solve_nqueens_util(board, row + 1, N, solutions)
             board[row][col] = 0
+
 
 def solve_nqueens(N):
     if not isinstance(N, int):
@@ -45,6 +53,7 @@ def solve_nqueens(N):
 
     for solution in solutions:
         print(solution)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
