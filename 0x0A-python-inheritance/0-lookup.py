0c7705e9e8b9ce7c8b22b8c4a07bcfd1c2d1216a
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-10-09 22:50:27 +0300

    Add lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
+
+def lookup(obj):
+    """Returns a list object"""
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,3 @@
+# 0x0A. Python - Inheritance
+
+0. Lookup
