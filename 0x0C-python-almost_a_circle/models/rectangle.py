54d5ba91b6576250b1c8734d049562c2578151d1
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-10-16 00:35:01 +0300

    Add area method to Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,6 +14,10 @@ class Rectangle(Base):
         self.__y = y
         super().__init__(id)
 
+    def area(self):
+        """calculate the area of the rectangle"""
+        return (self.__width * self.__height)
+
     @property
     def width(self):
         """get the width"""
@@ -22,7 +26,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """set the width"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -36,7 +40,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """set he height"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -50,10 +54,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """set x"""
-        if type(value) != int:
-           raise TypeError("x must be an integer")
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
         if value < 0:
-           raise ValueError("x must be >= 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -64,7 +68,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """set y"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
