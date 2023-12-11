17c7dc520bea08b7cc16b5693e517b2d927fb861
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-12-12 00:07:26 +0200

    Add factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function factorial (number) {
+  if (number < 0) {
+    return (-1);
+  }
+  if (number === 0 || isNaN(number)) {
+    return (1);
+  }
+  return (number * factorial(number - 1));
+}
+
+console.log(factorial(Number(process.argv[2])));
