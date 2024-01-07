873cee8b21908543a4ed2026117a14c7b9a88e12
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-01-08 01:08:41 +0200

    Fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,7 +5,8 @@ import requests
 
 if __name__ == "__main__":
     letter = "" if len(argv) == 1 else argv[1]
-    req = requests.post("http://741e42b6cc21.7177cab4.alx-cod.online:5000/search_user", {"q": letter})
+    payload = {"q": letter}
+    req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
 
     try:
         response = req.json()
