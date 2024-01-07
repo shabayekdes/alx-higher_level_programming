7c5faf74e929110a8d5e11d2167826f8744e5c5d
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-01-08 01:06:29 +0200

    Update url

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,7 +5,7 @@ import requests
 
 if __name__ == "__main__":
     letter = "" if len(argv) == 1 else argv[1]
-    req = requests.post("http://0.0.0.0:5000/search_user", {"q": letter})
+    req = requests.post("http://741e42b6cc21.7177cab4.alx-cod.online:5000/search_user", {"q": letter})
 
     try:
         response = req.json()
