93530d8f4d00ca5b89ca832a3129c6d93ae4508d
Esmail Shabayek <esmail.shabayek@gmail.com>
2024-02-08 01:54:25 +0200

    Add scripts

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $.get("https://swapi.co/api/films/?format=json", function (data) {
+    $("UL#list_movies").append(
+      ...data.results.map((movie) => `<li>${movie.title}</li>`)
+    );
+  });
+});
